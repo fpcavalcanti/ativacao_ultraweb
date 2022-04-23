e5b4cc674d8d14fe13a225077fabc31398c2eaad
Felipe P. Cavalcanti <felipe.cavalcanti@ebstecnologica.com.br>
2022-04-23 10:24:46 -0300

    continuação dev. sistema

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
 web_service_version = 0.1
+web_service_release_date = '22/04/2022'
 
 @app.route("/")
 @app.route("/ping")
 def req_ping():
-    return jsonify({ 'status': 'Ativo', 'mensagem': f'Web service executando v:{web_service_version}'})
+    return jsonify({ 'status': 'Ativo', 'mensagem': f'Web service executando v:{web_service_version} - Release: {web_service_release_date}'})
 
-@app.route("/do_ativa", methods=['GET', 'POST'])
+@app.route("/do_ativacao", methods=['POST'])
 def do_ativacao():
-    return jsonify({'status': True, 'received': 'ok'})
+    json_body = request.get_json(force=True)
+    print(json_body)
+    return jsonify({'status': True, 'received': json_body})
